ec87db7fab9493801de028cb295964aea1d8177f
Satish <119107590+SatishReddyKalli@users.noreply.github.com>
2023-11-17 07:35:23 +0530

    Update query.sql

--- sqlQuestion2/query.sql
+++ sqlQuestion2/query.sql
@@ -13,7 +13,7 @@ values ('A','Bangalore','A@gmail.com',1,'CPU'),('A','Bangalore','A1@gmail.com',1
 --Question 
 --Need to find most visited floor,total_visits, resources used
 with resources as (select name,count(1) as total_visits,
-				  string_agg(resources,',') as resources from entries group by name),
+				  string_agg(distinct resources,',') as resources from entries group by name),
 floor_visit as(select name,floor,count(1) as no_of_floor_visit,rank() over(
 partition by name order by count(1) desc) from entries group by name,floor)
 select t1.name,t1.total_visits,t1.resources,t2.floor as most_visited_floor
